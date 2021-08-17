cb4109981c920700bc856254ec0a0654b02fb07e
chehuy97 <chehuy97@gmail.com>
2021-08-17 18:48:36 +0700

    custom article view card and import search

--- src/components/article-item/article-item.component.tsx
+++ src/components/article-item/article-item.component.tsx
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.css";
 import { Article } from "../../redux/acticle/article.type";
-import './article-item.scss'
+import './article-item.styles.scss'
 
 type ArticleProps  = {
     article: Article
@@ -8,8 +8,8 @@ type ArticleProps  = {
 
 const ArticleItem = ({article}:ArticleProps) => {
   return (
-    <li className="media" key={article.id}>
-      <img src={article.image} className="mr-3" alt="..." />
+    <li className="media card" key={article.id}>
+      <img src={article.image} className="mr-3 card__image" alt="..." />
       <div className="media-body">
         <h5 className="mt-0 mb-1">{article.title}</h5>
         {article.content}
--- src/pages/article/Acticle.component.tsx
+++ src/pages/article/Acticle.component.tsx
@@ -1,28 +1,56 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import { useDispatch } from "react-redux";
 import { useSelector } from "../../redux/root-reducer";
 import { fetchArticles } from "../../redux/acticle/article.action";
-import ArticleItem from '../../components/article-item/article-item.component'
+import ArticleItem from "../../components/article-item/article-item.component";
+import "./article.styles.scss";
 
 const Acticle = () => {
   const dispatch = useDispatch();
-  const articles = useSelector(state => state.article.articles)
-  const listArticles = articles.map( item => <ArticleItem article={item}/>)
+  const articles = useSelector((state) => state.article.articles);
+  const [searchValue, setSearchValue] = useState("");
+  const [filterArticles, setFilterArticles] = useState(articles);
+  const listArticles =
+    searchValue === ""
+      ? articles.map((item) => <ArticleItem article={item} />)
+      : filterArticles.map((   item) => <ArticleItem article={item} />);
 
   useEffect(() => {
-    fetchListArticles()
-  },[]);
+    fetchListArticles();
+  }, []);
 
   const fetchListArticles = async () => {
     await dispatch(fetchArticles());
-  }
+  };
+
+  const filterListArticles = (value:string) => {
+    setSearchValue(value)
+    let filters = articles.filter(
+      (item) =>
+        item.title.includes(value) || item.content.includes(value)
+    );
+    setFilterArticles(filters);
+  };
 
   return (
     <div>
-      <ul className="list-unstyled">
-        {listArticles}
-      </ul>
+      <div className="search">
+        <input
+          className="form-control mr-sm-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+          value={searchValue}
+          onChange={(e) => filterListArticles(e.target.value)}
+        />
+        <button
+          className="btn btn-outline-success my-2 my-sm-0"
+          type="button">
+          Search
+        </button>
+      </div>
+      <ul className="list-unstyled">{listArticles}</ul>
     </div>
   );
 };
